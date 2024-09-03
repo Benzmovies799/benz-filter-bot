816aaa7e88e6d447bb62438919b2b31b9fce83c8
Benzmovies799 <128473684+Benzmovies799@users.noreply.github.com>
2024-09-03 10:31:33 +0530

    Update botHandeling.py

--- plugins/components/botHandeling.py
+++ plugins/components/botHandeling.py
@@ -55,9 +55,9 @@ async def set_stream_link(client, message):
             await db.set_stream_link(link)
             await message.reply("Successfully set stream link!")
         else:
-            await message.reply("Usage: /stream https://t.me/bisal_files or http://t.me/bisal_files")
+            await message.reply("Usage: /stream https://t.me/benzmovies or http://t.me/benzmovies")
     except IndexError:
-        await message.reply("Usage: /stream https://t.me/bisal_files or http://t.me/bisal_files")
+        await message.reply("Usage: /stream https://t.me/benzmovies or http://t.me/benzmovies")
         return
 
 
@@ -98,7 +98,7 @@ async def m_grp(client, message):
             await message.reply("Invalid link")
     link1 = await client.ask(message.chat.id ,"send me your movies grp link or send /skiplink to skip . default is bisal_files")
     if link1.text == "/skiplink":
-        links.append("https://t.me/bisal_files")
+        links.append("https://t.me/benzmovies")
     else:
         if checkIfLinkIsValid(link1.text):
             links.append(link1.text)
